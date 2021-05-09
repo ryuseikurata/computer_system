bae97f1745f1586703e6ff3631850fbc3ce7aa7a
シュレック倉田 <32031454+ryuseikurata@users.noreply.github.com>
2021-05-10 08:30:20 +0900

    feat: add test

--- src/nand.rs
+++ src/nand.rs
@@ -1,41 +1,23 @@
-fn nand(a: i8, b: i8) -> i8 {
-    if a == 0 && b == 0 {
-        0
-    } else {
-        1
-    }
+// 0: false
+// 1: true
+pub fn nand(a: bool, b: bool) -> bool {
+    !(a && b)
 }
 
-fn not(input: i8) -> i8 {
-    if(input == 0) {
-        1
-    } else {
-        0
-    }
+fn not(input: bool) -> bool {
+    nand(input, input)
 }
 
-fn and(a: i8, b:i8)-> i8 {
-    if a == 0 && b == 0 {
-        1
-    } else {
-        0
-    }
+fn and(a: bool, b:bool)-> bool {
+    !nand(a, b)
 }
 
-fn or(a: i8, b:i8) -> i8 {
-    if a == 0 && b == 0 {
-        0
-    } else {
-        1
-    }
+fn or(a: bool, b:bool) -> bool {
+    nand(!a, !b)
 }
 
-fn xor(a: i8, b: i8) -> i8 {
-    if a != b {
-        0
-    } else {
-        1
-    }
+fn xor(a: bool, b: bool) -> bool {
+    nand(a, b)
 }
 
 fn mux(a: i8, b:i8, sel: i8) -> i8 {
@@ -51,5 +33,24 @@ fn dmux(input: i8, sel: i8) -> (i8,i8) {
         (input, 0)
     } else {
         (0, input)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_nand() {
+        assert_eq!(nand(true, true), false);
+    }
+
+    #[test]
+    fn test_and() {
+        assert_eq!(or(false, false), false);
+    }
+
+    #[test]
+    fn test_xor() {
+        assert_eq!(xor(a, b))
     }
 }
