018ad020bc5a0c63dce967da5c4d0524d389ed56
シュレック倉田 <32031454+ryuseikurata@users.noreply.github.com>
2021-05-22 11:27:56 +0900

    refactor: mux

--- src/gates/mux.rs
+++ src/gates/mux.rs
@@ -1,16 +1,23 @@
+use super::{and, not, or};
+
 pub fn calc(a: bool, b: bool, sel: bool) -> bool {
-  if sel == false {
-    a
-  } else {
-    b
-  }
+    let result1 = and::calc(a, not::calc(sel));
+    let result2 = and::calc(b, sel);
+    or::calc(result1, result2)
 }
 
 #[cfg(test)]
 mod tests {
-  use super::*;
-  #[test]
-  fn test_mux() {
-    assert_eq!(calc(true, false, true), false)
-  }
+    use super::*;
+    #[test]
+    fn test_mux() {
+        assert_eq!(calc(false, false, false), false);
+        assert_eq!(calc(false, false, true), false);
+        assert_eq!(calc(true, false, true), false);
+        assert_eq!(calc(true, false, false), true);
+        assert_eq!(calc(false, true, true), true);
+        assert_eq!(calc(false, true, false), false);
+        assert_eq!(calc(true, true, true), true);
+        assert_eq!(calc(true, true, false), true);
+    }
 }
