bfc33bce11469976b1164f7282d91e6dd0f30790
シュレック倉田 <32031454+ryuseikurata@users.noreply.github.com>
2021-05-22 18:09:44 +0900

    feat: add dmux ways

--- src/gates/mod.rs
+++ src/gates/mod.rs
@@ -1,10 +1,12 @@
-pub mod nand;
 pub mod and;
-pub mod not;
-pub mod or;
-pub mod xor;
-pub mod mux;
 pub mod dmux;
+pub mod dmux4way;
+pub mod dmux8way;
+pub mod mux;
 pub mod mux16;
 pub mod mux4way16;
 pub mod mux8way16;
+pub mod nand;
+pub mod not;
+pub mod or;
+pub mod xor;
