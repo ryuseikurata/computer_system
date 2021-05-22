42c34f3afa3dcf67d44e630ac81852e086f30ba6
シュレック倉田 <32031454+ryuseikurata@users.noreply.github.com>
2021-05-22 11:13:03 +0900

    feat: add bit register

---
+++ src/sequence_circuits/bit_register.rs
@@ -0,0 +1,22 @@
+use crate::gates::mux;
+use super::flip_flop::FlipFlop;
+
+pub struct BitRegister {
+    flipflop: FlipFlop,
+}
+
+impl BitRegister {
+    pub fn new() -> Self {
+        Self {
+            flipflop: FlipFlop::new(),
+        }
+    }
+
+    pub fn out(&self) -> bool {
+      self.flipflop.out()
+    }
+
+    pub fn clock(&mut self, input: bool, load: bool) {
+      self.flipflop.clock(mux::calc(self.out(), input, load))
+    }
+}
