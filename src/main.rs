cf413f99a9690dd9bc060ab881353693023ef4d4
シュレック倉田 <32031454+ryuseikurata@users.noreply.github.com>
2021-05-22 11:04:15 +0900

    fix: flip_flop

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod gates;
 mod adders;
+mod sequence_circuits;
 
 fn main() {
     println!("Hello, world!");
---
+++ src/sequence_circuits/flip_flop.rs
@@ -0,0 +1,17 @@
+pub struct FlipFlop {
+    bit: bool,
+}
+
+impl FlipFlop {
+    pub fn new() -> FlipFlop {
+        FlipFlop { bit: false }
+    }
+
+    pub fn out(&self) -> bool {
+        self.bit
+    }
+
+    pub fn clock(&mut self, a: bool) {
+        self.bit = a
+    }
+}
