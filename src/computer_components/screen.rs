af34e912656e5df084eb9fda3f7d6b93332a7074
シュレック倉田 <32031454+ryuseikurata@users.noreply.github.com>
2021-06-06 13:27:15 +0900

    feat: add screen

--- src/computer_components/screen.rs
+++ src/computer_components/screen.rs
@@ -1,4 +1,25 @@
+use crate::sequence_circuits::{random_access_memory::RAM8K, word::Word};
 
 pub trait Screen {
     fn new() -> Self;
+    fn out(&self, address: [bool; 13]) -> Word;
+    fn clock(&mut self, address: [bool; 13], input: Word, load: bool);
+}
+
+pub struct DummyScreen {
+    ram: RAM8K,
+}
+
+impl Screen for DummyScreen {
+    fn new() -> Self {
+        Self { ram: RAM8K::new() }
+    }
+
+    fn out(&self, address: [bool; 13]) -> Word {
+        self.ram.out(address)
+    }
+
+    fn clock(&mut self, address: [bool; 13], input: Word, load: bool) {
+        self.ram.clock(address, input, load);
+    }
 }
