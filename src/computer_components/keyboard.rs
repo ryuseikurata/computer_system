21bca46f816dc62bac0890d99cc49b5b7bdffc2f
シュレック倉田 <32031454+ryuseikurata@users.noreply.github.com>
2021-06-06 13:27:07 +0900

    feat: add keyboard

--- src/computer_components/keyboard.rs
+++ src/computer_components/keyboard.rs
@@ -1,3 +1,27 @@
+use crate::sequence_circuits::{register::Register, word::Word};
+
 pub trait Keyboard {
+    type State;
     fn new() -> Self;
+    fn out(&self) -> Word;
+    fn set_state(&mut self, state: Self::State);
+}
+
+pub struct DummyKeyboard {
+    register: Register,
+}
+
+impl Keyboard for DummyKeyboard {
+    type State = ();
+    fn new() -> Self {
+        Self {
+            register: Register::new(),
+        }
+    }
+
+    fn out(&self) -> Word {
+        self.register.out()
+    }
+
+    fn set_state(&mut self, state: Self::State) {}
 }
